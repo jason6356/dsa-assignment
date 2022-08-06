63c6bbaf9894e6c1d7e609cce8ffe448ae07f275
jason6356 <xiaodemon6536@gmail.com>
2022-08-07 02:03:11 +0800

    adding classes and interface

---
+++ DSA-Assignment/src/adt/List.java
@@ -0,0 +1,3 @@
+package adt;
+
+public class List{}
---
+++ DSA-Assignment/src/client/Driver.java
@@ -0,0 +1,7 @@
+package client;
+
+public class Driver {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
